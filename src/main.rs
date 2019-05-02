771ae47740188dd7905e12fe29ade280bb1a6c80
jonathan gerber <jlgerber@gmail.com>
2019-05-02 16:00:43 -0700

    get manifest

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,45 @@
 
 
 use yamly::*;
-use std::io::Read;
-use serde_yaml ;
-use serde::{Deserialize, };
-use std::collections::BTreeMap ;
-use serde_yaml::Value as YamlValue;
-pub mod build_request;
-type ManifestMap = BTreeMap<String, YamlValue>;
 
-#[derive(Deserialize,Debug)]
-struct Manifest {
+use shellfn::shell;
+use std::error::Error;
+use serde_json::{Value};
+use serde::{Deserialize, Serialize};
+use std::iter::Iterator;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Flavour {
     name: String,
+    version: String,
 }
 
-const VAL:  &'static str = r#"
----
-name: foobar
-version: 1
-description: The coolest app ever
-requires:
-    foo: '1.0.0+<3'
-    bar: '2.0.0+'
-"#;
-
-pub enum ManifestType {
-    Str(&'static str),
-    File(String),
+#[derive(Debug, Deserialize, Serialize)]
+struct Flavours {
+    flavours: Vec<Flavour>
 }
 
-fn package_from_manifest(manifest: &str) -> Option<String> {
-    match serde_yaml::from_reader(manifest.as_bytes()){
-        Ok(val) => Some(val),
-        Err(_) => None
-    }
+#[derive(Debug, Deserialize, Serialize)]
+struct Manifests {
+    manifests: Vec<Flavours>
 }
 
+#[shell]
+fn _get_flavors() -> Result<impl Iterator<Item=String>, Box<Error>> { r#"
+    pk manifest --flavours --name --json=1
+"# }
 
-fn main() {
-    let some_package = package_from_manifest(VAL);
-    println!("{:?}", some_package);
+pub fn get_flavors() -> Result<Vec<String>, Box<Error>> {
+    let mut flavors = _get_flavors()?;
+    let flavors = flavors.next().unwrap();
+    let flavors: Manifests = serde_json::from_str(flavors.as_str())?;
+    let result = flavors.manifests[0].flavours.iter().map(|flav| flav.name.to_string() ).collect::<Vec<String>>();
+    Ok(result)
+}
+
+fn main() -> Result<(), Box<Error>>{
+    let flavors = get_flavors()?;
+
+    println!("{:?}", flavors);
+    Ok(())
 }

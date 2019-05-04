a7a130ac3fdf73d613303f10013f1ce874ebc2d0
jonathan gerber <jlgerber@gmail.com>
2019-05-04 13:18:06 -0700

    adds optional path to flavors

--- src/flavor.rs
+++ src/flavor.rs
@@ -20,12 +20,16 @@ struct Manifests {
 }
 
 #[shell]
-fn _get_flavors() -> Result<impl Iterator<Item=String>, failure::Error> { r#"
-    pk manifest --flavours --json=1
+fn _get_flavors(flavor_path: &str) -> Result<impl Iterator<Item=String>, failure::Error> { r#"
+    cd $FLAVOR_PATH && pk manifest --flavours --json=1
 "# }
 
-pub fn get_flavors() -> Result<Vec<String>, failure::Error> {
-    let mut flavors = _get_flavors()?;
+pub fn get_flavors(path: Option<&Path>) -> Result<Vec<String>, failure::Error> {
+    let default_path = ".";
+    let path = path.unwrap_or_else(|| Path::new(default_path));
+    let path = path.as_os_str().to_str().ok_or_else(|| RemoteBuildError::ConversionError("unable to convert path to str".to_string()))?;
+
+    let mut flavors = _get_flavors(path)?;
     let flavors = flavors.next().unwrap();
     let flavors: Manifests = serde_json::from_str(flavors.as_str())?;
     let result = flavors.manifests[0].flavours.iter().map(|flav| flav.name.to_string() ).collect::<Vec<String>>();

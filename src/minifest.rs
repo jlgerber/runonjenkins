a2524028af532d0c972c3f7a80f3510a4b24b651
jonathan gerber <jlgerber@gmail.com>
2019-05-04 11:43:01 -0700

    adds optional path to from_disk method. adds tests

--- src/minifest.rs
+++ src/minifest.rs
@@ -1,5 +1,6 @@
 use shellfn::shell;
-use crate::ShellFnError;
+use crate::{ShellFnError, RemoteBuildError};
+use std::path::{Path};
 
 // for some odd reason, pk manifest --field does not work with --json flag
 #[shell]
@@ -8,21 +9,23 @@ fn _get_minifest() -> Result<impl Iterator<Item=String>, failure::Error> { r#"
 "#
 }
 
+// pk manifest is quite a bit slowe than using unix commands to grab the version
+// and name from the manifest
 #[cfg(target_os = "macos")]
 #[shell]
-fn _get_minifest_from_grep() -> Result<impl Iterator<Item=String>, failure::Error> { r#"
-    gfind . -regextype posix-egrep -regex '.*(manifest|pk)\.yaml' | xargs -I@ grep -iE '^version:|^name:' @ | sed s/\'//g | sed 's/ //g'
+fn _get_minifest_from_grep(minifest_root_dir: &str) -> Result<impl Iterator<Item=String>, failure::Error> { r#"
+    cd $MINIFEST_ROOT_DIR && gfind . -regextype posix-egrep -regex '.*(manifest|pk)\.yaml' | xargs -I@ grep -iE '^version:|^name:' @ | sed s/\'//g | sed 's/ //g'
 "#
 }
 
 #[cfg(target_os = "linux")]
 #[shell]
-fn _get_minifest_from_grep() -> Result<impl Iterator<Item=String>, failure::Error> { r#"
-    find . -regextype posix-egrep -regex '.*(manifest|pk)\.yaml' | xargs -I@ grep -iE '^version:|^name:' @ | sed s/\'//g | sed 's/ //g'
+fn _get_minifest_from_grep(minifest_root_dir: &str) -> Result<impl Iterator<Item=String>, failure::Error> { r#"
+    cd $MINIFEST_ROOT_DIR && find . -regextype posix-egrep -regex '.*(manifest|pk)\.yaml' | xargs -I@ grep -iE '^version:|^name:' @ | sed s/\'//g | sed 's/ //g'
 "#
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
 /// The mini manifest - just the name and version because that is all we need.
 pub struct Minifest {
     pub name:    String,
@@ -30,15 +33,22 @@ pub struct Minifest {
 }
 
 impl Minifest {
-    pub fn new(name: String, version: String) -> Self {
+    pub fn new<I: Into<String>>(name: I, version: I) -> Self {
         Self {
-            name, version
+            name: name.into(),
+            version: version.into(),
         }
     }
 
-    /// retrieve the Minifest from disk
-    pub fn from_disk() -> Result<Minifest, failure::Error> {
-        let mut mmiter = _get_minifest_from_grep()?;
+    /// retrieve the Minifest from disk, assuming our CWD is in a
+    /// project with a manifest.
+    pub fn from_disk(path: Option<&Path>) -> Result<Minifest, failure::Error> {
+        // convert path to &str
+        let default_path = ".";
+        let path = path.unwrap_or_else(|| Path::new(default_path));
+        let path = path.as_os_str().to_str().ok_or_else(|| RemoteBuildError::ConversionError("unable to convert path to str".to_string()))?;
+        let mut mmiter = _get_minifest_from_grep(path)?;
+
         let mut attrs = Vec::with_capacity(2);
         attrs.push( mmiter.next().ok_or_else(|| ShellFnError("unable to get name from manifest".to_string()))? );
         attrs.push(mmiter.next().ok_or_else(|| ShellFnError("Unable to get version from manifest".to_string()))?);
@@ -57,13 +67,39 @@ impl Minifest {
 
         Ok(Minifest::new(name,version))
     }
-    // /// retrieve the Minifest from disk
-    // pub fn from_disk() -> Result<Minifest, failure::Error> {
-    //     let mut mmiter = _get_minifest()?;
-    //     let name =  mmiter.next().ok_or_else(|| ShellFnError("unable to get name from manifest".to_string()))?;
-    //     let version = mmiter.next().ok_or_else(|| ShellFnError("Unable to get version from manifest".to_string()))?;
-    //     Ok(Minifest::new(name,version))
-    // }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    //use tempfile::tempdir;
+    use std::fs::File;
+    use std::io::Write;
+    const MANIFEST: &'static str = r#"
+---
+name: 'Fred'
+Version: '0.1.0'
+Description: Other stuff
+"#;
+
+    #[test]
+    fn can_fetch_from_disk() {
+        let dir = std::env::temp_dir();//.expect("could not create tempdir in test");
+        let mut file_path = dir.clone();
+        file_path.push("manifest.yaml");
+        let mut file = File::create(&file_path).expect("could not create tempfile");
+        writeln!(file, "{}", MANIFEST).expect("could not write MANIFEST to tempfile");
+
+        let minifest = Minifest::from_disk(Some(&dir))
+            .expect(format!("could not unwrap minifest in test: {:?}",dir ).as_str());
+        let expected = Minifest::new("Fred", "0.1.0");
+
+        // remove file
+        let _ = std::fs::remove_file(&file_path);
+
+        assert_eq!(minifest, expected);
+
+
+    }
+}
 

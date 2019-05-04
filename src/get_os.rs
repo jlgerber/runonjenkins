980bbee5dbdec77cc841bdc2c042b8cc5f8b95a1
jonathan gerber <jlgerber@gmail.com>
2019-05-04 12:13:22 -0700

    adds OS_VAR name to constants and MachineOs relies on this to fetch the os from the env

--- src/get_os.rs
+++ src/get_os.rs
@@ -1,17 +1,18 @@
 use shellfn::shell;
-use crate::ShellFnError;
+use crate::{ShellFnError, constants::OS_VAR;
 
-
+/// Retrieve the os from the shell
 pub struct MachineOs;
 
 impl MachineOs {
+    /// Retreive the os from the environment
     pub fn get_from_env() -> String {
-        let my_os = _get_os();
+        let my_os = _get_os(OS_VAR);
     }
 }
 
 #[shell]
-fn _get_os() -> Result<String, failure::Error> { r#"
-    printenv DD_OS
+fn _get_os(os_var: &str) -> Result<String, failure::Error> { r#"
+    printenv $OS_VAR
 "#
 }

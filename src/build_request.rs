4a9727a01be167c7523fea3ce52f0c8076a67e82
jonathan gerber <jlgerber@gmail.com>
2019-05-04 07:56:21 -0700

    deletes UrlEncodeable struct in build_request.rs

--- src/build_request.rs
+++ src/build_request.rs
@@ -93,36 +93,6 @@ impl BuildParameters {
     }
 }
 
-#[derive(Debug, PartialEq,PartialOrd,Eq,Ord,Serialize)]
-pub struct UrlEncodeable {
-    project:  String,
-    version:  String,
-    flavor:   String,
-    repo:     String,
-    #[serde(rename = "scmType")]
-    scm_type: String,
-    platform: String,
-}
-
-impl UrlEncodeable {
-    pub fn new <I: Into<String> >(
-        project: I,
-        version: I,
-        flavor: I,
-        repo: I,
-        scm_type: I,
-        platform: I ) -> UrlEncodeable
-    {
-        Self {
-            project: project.into(),
-            version: version.into(),
-            flavor: flavor.into(),
-            repo: repo.into(),
-            scm_type: scm_type.into(),
-            platform: platform.into(),
-        }
-    }
-}
 #[derive(Debug, PartialEq,PartialOrd,Eq,Ord)]
 /// The user facing request object. This is converted to the more cumbersome BuildParameters
 /// object in order to serialize to json for the actual build request POST.
@@ -161,7 +131,7 @@ impl BuildRequest {
         })
     }
 
-    /// generate a BuildParameters struct from a BuildRequest. The BuildParameters
+    /// Generate a BuildParameters struct from a BuildRequest. The BuildParameters
     /// is json serializable and has the correct shape
     pub fn to_build_params(&self) -> BuildParameters {
         let mut params = BuildParameters::new();
